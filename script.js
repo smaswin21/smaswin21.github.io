c175f1ec7d43b3ec0bf30ab31035a69a1fa202ab
smaswin21 <sm.aswin21@gmail.com>
2023-12-25 11:24:03 +0100

    commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// Function to handle dropdown visibility
+function toggleDropdown() {
+    var dropdownContent = document.getElementById("hobbiesDropdown");
+    if (dropdownContent.style.display === "block") {
+        dropdownContent.style.display = "none";
+    } else {
+        dropdownContent.style.display = "block";
+    }
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.style.display === "block") {
+                openDropdown.style.display = "none";
+            }
+        }
+    }
+}
